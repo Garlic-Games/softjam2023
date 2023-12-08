86593645ebd526334bb563b6de0fb7c960711882
bocdagla <tomeu1m1a1@gmail.com>
2023-12-08 17:42:24 +0100

    Added state transition on heat

--- Scripts/StateMachine/StateMachine.cs
+++ Scripts/StateMachine/StateMachine.cs
@@ -5,37 +5,70 @@ public enum GameStates { Intro = 0, Calm = 1, Warning = 2, Danger = 3, GameOver 
 
 public partial class StateMachine : Node
 {
-	[Signal]
-	public delegate void StateChangedEventHandler(int state);
+    [Export]
+    private Plant _plant;
+    [Export]
+    private Temperatura _temperatura;
+
+    [Signal]
+    public delegate void StateChangedEventHandler(int state);
 
     public GameStates State { get; private set; } = GameStates.GameOver;
 
     public override void _Ready()
     {
         ChangeState(GameStates.Intro);
+        if (_temperatura != null)
+        {
+            _temperatura.CalmTemperatureReached += BeCalm;
+            _temperatura.WarningTemperatureReached += BeNervous;
+            _temperatura.DangerTemperatureReached += BeFreakedOut;
+        }
+    }
+
+    private void BeFreakedOut()
+    {
+        ChangeState(GameStates.Danger);
+    }
+
+    private void BeNervous()
+    {
+        ChangeState(GameStates.Warning);
+    }
+
+    private void BeCalm()
+    {
+        ChangeState(GameStates.Calm);
     }
 
     public void ChangeState(GameStates newState)
-	{
-		if(State == newState) return;
-		
-		switch(newState)
-		{
-			case GameStates.Intro:
-				MusicManager.Instance.PlayIntro(); 
-				break;
-            case GameStates.Calm:
+    {
+        if (State == newState) return;
+
+        switch (newState)
+        {
+            case GameStates.Intro:
                 MusicManager.Instance.PlayIntro();
+                break;
+            case GameStates.Calm:
+                MusicManager.Instance.PlayCalmLoop();
                 break;
             case GameStates.Warning:
-                MusicManager.Instance.PlayIntro();
+                if (State == GameStates.Danger)
+                {
+                    MusicManager.Instance.PlayBackFromDanger();
+                }
+                else
+                {
+                    MusicManager.Instance.PlayWarning();
+                }
                 break;
             case GameStates.Danger:
-                MusicManager.Instance.PlayIntro();
+                MusicManager.Instance.PlayDanger();
                 break;
         }
         State = newState;
-		
-		EmitSignal(SignalName.StateChanged, (int)newState);
-	}
+
+        EmitSignal(SignalName.StateChanged, (int)newState);
+    }
 }
