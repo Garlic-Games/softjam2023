2e8fbf37ec8b7ef3df0c847744ef03bdae3dc412
Guillem <tukav73@gmail.com>
2023-12-08 02:08:13 +0100

    Map more keys to player movement

--- Scripts/Player/MainPlayerController.cs
+++ Scripts/Player/MainPlayerController.cs
@@ -23,7 +23,11 @@ public partial class MainPlayerController : CharacterBody3D
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector2 inputDir = Input.GetVector(
+			"player_movement_left", 
+			"player_movement_right", 
+			"player_movement_forward", 
+			"player_movement_backward");
 		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
 		if (direction != Vector3.Zero)
 		{
