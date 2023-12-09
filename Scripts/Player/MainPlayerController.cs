9df33e7908ee0d62bae1e888b2095513bd22b21a
Soteck <tukav73@gmail.com>
2023-12-09 22:03:23 +0100

    Add sprint

--- Scripts/Player/MainPlayerController.cs
+++ Scripts/Player/MainPlayerController.cs
@@ -10,6 +10,7 @@ public partial class MainPlayerController : CharacterBody3D
 	public Camera3D camera;
 	
 	public const float Speed = 5.0f;
+	public const float SprintSpeed = 7.0f;
 	public const float JumpVelocity = 4.5f;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -23,8 +24,8 @@ public partial class MainPlayerController : CharacterBody3D
 		rifleMesh = rifleContainer.GetChild<Rifle>(0);
 	}
 
-	public override void _PhysicsProcess(double delta)
-	{
+	public override void _PhysicsProcess(double delta) {
+		float speed = Speed;
 		Vector3 velocity = Velocity;
 
 		// Add the gravity.
@@ -32,9 +33,14 @@ public partial class MainPlayerController : CharacterBody3D
 			velocity.Y -= gravity * (float)delta;
 
 		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor()) {
 			velocity.Y = JumpVelocity;
+		}
 
+		if (Input.IsActionPressed("player_sprint")) {
+			speed = SprintSpeed;
+		}
+		
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 inputDir = Input.GetVector(
@@ -45,13 +51,13 @@ public partial class MainPlayerController : CharacterBody3D
 		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
 		if (direction != Vector3.Zero)
 		{
-			velocity.X = direction.X * Speed;
-			velocity.Z = direction.Z * Speed;
+			velocity.X = direction.X * speed;
+			velocity.Z = direction.Z * speed;
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, speed);
 		}
 
 		Velocity = velocity;
