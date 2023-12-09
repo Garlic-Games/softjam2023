16116188a36ff986afc34abd5dc783e0ce270954
Soteck <tukav73@gmail.com>
2023-12-09 21:05:36 +0100

    Got wrong code here

--- ExternalAssets/Godot-3D-Lines/DrawLine3D.gd
+++ ExternalAssets/Godot-3D-Lines/DrawLine3D.gd
@@ -4,20 +4,20 @@ class Line:
 	var Start
 	var End
 	var LineColor
-	var _Time
+	var time
 	
-	func _init(Start, End, LineColor, _Time):
+	func _init(Start, End, LineColor, time):
 		self.Start = Start
 		self.End = End
 		self.LineColor = LineColor
-		self._Time = _Time
+		self.time = time
 
 var Lines = []
 var RemovedLine = false
 
 func _process(delta):
 	for i in range(len(Lines)):
-		Lines[i]._Time -= delta
+		Lines[i].time -= delta
 	
 	if(len(Lines) > 0 || RemovedLine):
 		queue_redraw() #Calls _draw
@@ -30,7 +30,7 @@ func _draw():
 		var ScreenPointEnd = Cam.unproject_position(Lines[i].End)
 		
 		#Dont draw line if either start or end is considered behind the camera
-		#this causes the line to not be drawn some_Times but avoids a bug where the
+		#this causes the line to not be drawn sometimes but avoids a bug where the
 		#line is drawn incorrectly
 		if(Cam.is_position_behind(Lines[i].Start) ||
 			Cam.is_position_behind(Lines[i].End)):
@@ -38,21 +38,21 @@ func _draw():
 		
 		draw_line(ScreenPointStart, ScreenPointEnd, Lines[i].LineColor)
 	
-	#Remove lines that have _Timed out
+	#Remove lines that have timed out
 	var i = Lines.size() - 1
 	while (i >= 0):
-		if(Lines[i]._Time < 0.0):
+		if(Lines[i].time < 0.0):
 			Lines.remove_at(i)
 			RemovedLine = true
 		i -= 1
 
-func DrawLine(Start, End, LineColor, _Time = 0.0):
-	Lines.append(Line.new(Start, End, LineColor, _Time))
+func DrawLine(Start, End, LineColor, time = 0.0):
+	Lines.append(Line.new(Start, End, LineColor, time))
 
-func DrawRay(Start, Ray, LineColor, _Time = 0.0):
-	Lines.append(Line.new(Start, Start + Ray, LineColor, _Time))
+func DrawRay(Start, Ray, LineColor, time = 0.0):
+	Lines.append(Line.new(Start, Start + Ray, LineColor, time))
 
-func DrawCube(Center, HalfExtents, LineColor, _Time = 0.0):
+func DrawCube(Center, HalfExtents, LineColor, time = 0.0):
 	#Start at the 'top left'
 	var LinePointStart = Center
 	LinePointStart.x -= HalfExtents
@@ -61,37 +61,37 @@ func DrawCube(Center, HalfExtents, LineColor, _Time = 0.0):
 	
 	#Draw top square
 	var LinePointEnd = LinePointStart + Vector3(0, 0, HalfExtents * 2.0)
-	DrawLine(LinePointStart, LinePointEnd, LineColor, _Time);
+	DrawLine(LinePointStart, LinePointEnd, LineColor, time);
 	LinePointStart = LinePointEnd
 	LinePointEnd = LinePointStart + Vector3(HalfExtents * 2.0, 0, 0)
-	DrawLine(LinePointStart, LinePointEnd, LineColor, _Time);
+	DrawLine(LinePointStart, LinePointEnd, LineColor, time);
 	LinePointStart = LinePointEnd
 	LinePointEnd = LinePointStart + Vector3(0, 0, -HalfExtents * 2.0)
-	DrawLine(LinePointStart, LinePointEnd, LineColor, _Time);
+	DrawLine(LinePointStart, LinePointEnd, LineColor, time);
 	LinePointStart = LinePointEnd
 	LinePointEnd = LinePointStart + Vector3(-HalfExtents * 2.0, 0, 0)
-	DrawLine(LinePointStart, LinePointEnd, LineColor, _Time);
+	DrawLine(LinePointStart, LinePointEnd, LineColor, time);
 	
 	#Draw bottom square
 	LinePointStart = LinePointEnd + Vector3(0, -HalfExtents * 2.0, 0)
 	LinePointEnd = LinePointStart + Vector3(0, 0, HalfExtents * 2.0)
-	DrawLine(LinePointStart, LinePointEnd, LineColor, _Time);
+	DrawLine(LinePointStart, LinePointEnd, LineColor, time);
 	LinePointStart = LinePointEnd
 	LinePointEnd = LinePointStart + Vector3(HalfExtents * 2.0, 0, 0)
-	DrawLine(LinePointStart, LinePointEnd, LineColor, _Time);
+	DrawLine(LinePointStart, LinePointEnd, LineColor, time);
 	LinePointStart = LinePointEnd
 	LinePointEnd = LinePointStart + Vector3(0, 0, -HalfExtents * 2.0)
-	DrawLine(LinePointStart, LinePointEnd, LineColor, _Time);
+	DrawLine(LinePointStart, LinePointEnd, LineColor, time);
 	LinePointStart = LinePointEnd
 	LinePointEnd = LinePointStart + Vector3(-HalfExtents * 2.0, 0, 0)
-	DrawLine(LinePointStart, LinePointEnd, LineColor, _Time);
+	DrawLine(LinePointStart, LinePointEnd, LineColor, time);
 	
 	#Draw vertical lines
 	LinePointStart = LinePointEnd
-	DrawRay(LinePointStart, Vector3(0, HalfExtents * 2.0, 0), LineColor, _Time)
+	DrawRay(LinePointStart, Vector3(0, HalfExtents * 2.0, 0), LineColor, time)
 	LinePointStart += Vector3(0, 0, HalfExtents * 2.0)
-	DrawRay(LinePointStart, Vector3(0, HalfExtents * 2.0, 0), LineColor, _Time)
+	DrawRay(LinePointStart, Vector3(0, HalfExtents * 2.0, 0), LineColor, time)
 	LinePointStart += Vector3(HalfExtents * 2.0, 0, 0)
-	DrawRay(LinePointStart, Vector3(0, HalfExtents * 2.0, 0), LineColor, _Time)
+	DrawRay(LinePointStart, Vector3(0, HalfExtents * 2.0, 0), LineColor, time)
 	LinePointStart += Vector3(0, 0, -HalfExtents * 2.0)
-	DrawRay(LinePointStart, Vector3(0, HalfExtents * 2.0, 0), LineColor, _Time)
+	DrawRay(LinePointStart, Vector3(0, HalfExtents * 2.0, 0), LineColor, time)
