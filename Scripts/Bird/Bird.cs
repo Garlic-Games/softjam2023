83e90388ca3418186f9cbbeebe6f32bdcdc0055f
Soteck <tukav73@gmail.com>
2023-12-10 17:00:11 +0100

    - Set birds on fire when a "hot threshold is reached"

--- Scripts/Bird/Bird.cs
+++ Scripts/Bird/Bird.cs
@@ -1,48 +1,90 @@
 using Godot;
 using System;
 
-public partial class Bird : Node3D
-{
+public partial class Bird : Node3D {
+    private enum TemperatureStatus {
+        Cool,
+        Medium,
+        Hot,
+        Fire
+    };
 
-	[Export] public BirdSky BirdSky;
 
-	private Node3D seekNode;
-	
-	private float Speed = 5f;
-	private Vector3 direction_forward = new Vector3(0, 0, -1);
-	private float lastDistance = 99999f;
-	private bool moving = false;
-	
-	public override void _Ready()
-	{
-		Node3D firstNode = BirdSky.getRandomNode();
-		GlobalTransform = firstNode.GlobalTransform;
-		NextPath();
-	}
+    [Export]
+    public BirdSky BirdSky;
 
-	private void NextPath()
-	{
-		seekNode = BirdSky.getRandomNode();
-		lastDistance = GlobalPosition.DistanceTo(seekNode.GlobalPosition);
-		LookAt(seekNode.GlobalPosition);
-		moving = true;
-	}
+    [Export]
+    public float MinCoolThreshold = 0f;
 
-	public override void _PhysicsProcess(double delta)
-	{
-		if (moving)
-		{
-			Translate(direction_forward.Normalized() * Speed * (float) delta);
-		}
-		float newDistance = GlobalPosition.DistanceTo(seekNode.GlobalPosition);
-		if (newDistance > lastDistance)
-		{
-			moving = false;
-			NextPath();
-		}
-		else
-		{
-			lastDistance = newDistance;
-		}
-	}
+    [Export]
+    public float MinMediumThreshold = 30f;
+
+    [Export]
+    public float MinHotThreshold = 60f;
+
+    [Export]
+    public float MinFireThreshold = 75f;
+
+    private Node3D seekNode;
+
+    private float Speed = 5f;
+    private Vector3 direction_forward = new Vector3(0, 0, -1);
+    private float lastDistance = 99999f;
+    private bool moving = false;
+    private TemperatureStatus _temperatureStatus = TemperatureStatus.Cool;
+
+    private WaterAffectedCollider _waterAffectedCollider;
+    private GpuParticles3D _fireParticles;
+
+    public override void _Ready() {
+        _waterAffectedCollider = (WaterAffectedCollider) FindChild("Hitbox");
+        _fireParticles = (GpuParticles3D) FindChild("FireParticles");
+        Node3D firstNode = BirdSky.getRandomNode();
+        GlobalTransform = firstNode.GlobalTransform;
+        NextPath();
+    }
+
+    private void NextPath() {
+        seekNode = BirdSky.getRandomNode();
+        lastDistance = GlobalPosition.DistanceTo(seekNode.GlobalPosition);
+        LookAt(seekNode.GlobalPosition);
+        moving = true;
+    }
+
+    public override void _PhysicsProcess(double delta) {
+        if (moving) {
+            Translate(direction_forward.Normalized() * Speed * (float) delta);
+        }
+
+        float newDistance = GlobalPosition.DistanceTo(seekNode.GlobalPosition);
+        if (newDistance > lastDistance) {
+            moving = false;
+            NextPath();
+        } else {
+            lastDistance = newDistance;
+        }
+
+        if (temperature > MinFireThreshold) {
+            _temperatureStatus = TemperatureStatus.Fire;
+        } else if (temperature > MinHotThreshold) {
+            _temperatureStatus = TemperatureStatus.Hot;
+        } else if (temperature > MinMediumThreshold) {
+            _temperatureStatus = TemperatureStatus.Medium;
+        } else {
+            _temperatureStatus = TemperatureStatus.Cool;
+        }
+
+        if (_temperatureStatus == TemperatureStatus.Fire) {
+            if (!_fireParticles.Emitting) {
+                _fireParticles.Emitting = true;
+            }
+        } else {
+            if (_fireParticles.Emitting) {
+                _fireParticles.Emitting = false;
+            }
+        }
+    }
+
+
+    public float temperature => _waterAffectedCollider.temperature;
 }
--- Scripts/Collider/WaterAffectedCollider.cs
+++ Scripts/Collider/WaterAffectedCollider.cs
@@ -4,61 +4,62 @@ using Softjam2023.Scripts.Autoload;
 using Softjam2023.Scripts.Util;
 
 public partial class WaterAffectedCollider : RigidBody3D {
+    [Export]
+    public float RatioMin = 1f;
 
-	[Export]
-	public float RatioMin = 1f;
-	[Export]
-	public float RatioMax = 2f;
-	
-	[Export]
-	public float TempLostPerHit = 7f;
+    [Export]
+    public float RatioMax = 2f;
 
-	private float _maxTemp = 100f;
-	private float _minTemp = 0f;
-	
-	private float _tempCount = 0f;
-	private float _ratio;
-	private TempShower _tempShower;
+    [Export]
+    public float TempLostPerHit = 7f;
 
-	public override void _Ready() {
-		_ratio = GD.Randf() * RatioMax + RatioMin;
-		if (Constants.DebugMode) {
-			ObjectInstanceProviderAutoLoad autoLoad = 
-				GetNode<ObjectInstanceProviderAutoLoad>("/root/ObjectInstanceProviderAutoLoad");
-			_tempShower = autoLoad.GimmeAProgressBarTempShower();
-		}
-	}
+    private float _maxTemp = 100f;
+    private float _minTemp = 0f;
 
-	public override void _Process(double delta) {
-		if (_tempCount < _maxTemp) {
-			_tempCount += _ratio * (float) delta;
-		}
+    private float _tempCount = 0f;
+    private float _ratio;
+    private TempShower _tempShower;
 
-		if (_tempCount > _maxTemp) {
-			_tempCount = _maxTemp;
-		}
+    public override void _Ready() {
+        _ratio = GD.Randf() * RatioMax + RatioMin;
+        if (Constants.DebugMode) {
+            ObjectInstanceProviderAutoLoad autoLoad =
+                GetNode<ObjectInstanceProviderAutoLoad>("/root/ObjectInstanceProviderAutoLoad");
+            _tempShower = autoLoad.GimmeAProgressBarTempShower();
+        }
+    }
 
-		if (_tempShower != null) {
-			Camera3D camera = GetViewport().GetCamera3D();
-			Vector3 position = GlobalPosition;
-			if (camera != null ) {
-				if (!camera.IsPositionBehind(position)) {
-					Vector2 position2d = camera.UnprojectPosition(GlobalPosition);
-					_tempShower.Show();
-					_tempShower.SetPosition(position2d);
-					_tempShower.SetBarPercent(_tempCount);
-				} else {
-					_tempShower.Hide();
-				}
+    public override void _Process(double delta) {
+        if (_tempCount < _maxTemp) {
+            _tempCount += _ratio * (float) delta;
+        }
 
-			}
-		}
-	}
+        if (_tempCount > _maxTemp) {
+            _tempCount = _maxTemp;
+        }
 
-	public void NotifyWaterCollision() {
-		_tempCount -= TempLostPerHit;
-		if (_tempCount < _minTemp) {
-			_tempCount = _minTemp;
-		}
-	}
+        if (_tempShower != null) {
+            Camera3D camera = GetViewport().GetCamera3D();
+            Vector3 position = GlobalPosition;
+            if (camera != null) {
+                if (!camera.IsPositionBehind(position)) {
+                    Vector2 position2d = camera.UnprojectPosition(GlobalPosition);
+                    _tempShower.Show();
+                    _tempShower.SetPosition(position2d);
+                    _tempShower.SetBarPercent(_tempCount);
+                } else {
+                    _tempShower.Hide();
+                }
+            }
+        }
+    }
+
+    public void NotifyWaterCollision() {
+        _tempCount -= TempLostPerHit;
+        if (_tempCount < _minTemp) {
+            _tempCount = _minTemp;
+        }
+    }
+
+    public float temperature => _tempCount;
 }
