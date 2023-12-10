0bd8c952ba9d9e3e3ff20d3ef2ef7de10e3ea5ab
Soteck <tukav73@gmail.com>
2023-12-10 11:41:30 +0100

    Fix projectile collision. Improve effects

--- Scripts/Player/WaterProjectile.cs
+++ Scripts/Player/WaterProjectile.cs
@@ -2,48 +2,60 @@ using Godot;
 
 namespace Softjam2023.Scripts.Player;
 
-public partial class WaterProjectile : RigidBody3D
-{
-    public enum Status
-    {
+public partial class WaterProjectile : RigidBody3D {
+    public enum Status {
         HIDDEN,
         IDLE,
         MOVING
     };
 
-    private const float TimeToLive = 7f; 
-    
+    private const float TimeToLive = 7f;
+
     private Status status = Status.IDLE;
     private float Speed = 50f;
     private float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
     private Vector3 direction_forward = new Vector3(0, 0, -1);
     private Vector3 direction_down = new Vector3(0, -1, 0);
+    private Vector3 _customVelocity;
 
     private float _lifeSpan = 0f;
+    private float _terminalVelocity = 53.0f;
 
+    public override void _Ready() {
+    }
 
     public override void _Process(double delta) {
         if (_lifeSpan > TimeToLive) {
-            GetParent().RemoveChild(this);
-            QueueFree();
+            Explode();
         }
     }
 
-    
-    public override void _PhysicsProcess(double delta)
-    {
-
-
-
+    public override void _PhysicsProcess(double delta) {
+        if (status == Status.MOVING) {
+            _lifeSpan += (float) delta;
+            if (_customVelocity.Y < _terminalVelocity) {
+                _customVelocity.Y += gravity * (float) delta * -1;
+            }
+            KinematicCollision3D collission = MoveAndCollide((_customVelocity * (float) delta));
+            if (collission != null) {
+                GD.Print(collission);
+                Explode();
+            }
+        }
     }
 
-    public void Shoot()
-    {
+    public void OnBodyShapeEnteredEventHandler(Rid body_rid, Node body, long body_shape_index, long local_shape_index) {
+        GD.Print(body_rid);
+    }
+
+    public void Shoot() {
         status = Status.MOVING;
         //Thanks to: https://forum.godotengine.org/t/godot-3-rotation-doesnt-effect-axis-for-velocity/29980
-        LinearVelocity = GlobalTransform.Basis.Orthonormalized() * direction_forward * Speed;
-
+        _customVelocity = GlobalTransform.Basis.Orthonormalized() * direction_forward * Speed;
     }
-    
-    
+
+    private void Explode() {
+        GetParent().RemoveChild(this);
+        QueueFree();
+    }
 }
