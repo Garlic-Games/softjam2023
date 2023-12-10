2eb5272a32be600165bd962c5e3b216d2b74109f
Soteck <tukav73@gmail.com>
2023-12-10 14:40:06 +0100

    Add debugMode in constants.

--- Scripts/Util/Constants.cs
+++ Scripts/Util/Constants.cs
@@ -1,6 +1,7 @@
 ﻿namespace Softjam2023.Scripts.Util; 
 
 public class Constants {
+    public const bool DebugMode = true;
     public static class CollisionLayers {
         public const int Default = 1;
         public const int Player = 2;
