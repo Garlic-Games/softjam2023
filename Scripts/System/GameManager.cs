ef072e4c42eb3463fb30b961791d8f73a6efd6ce
Soteck <tukav73@gmail.com>
2023-12-11 23:31:29 +0100

    Add start/splash menu before loading main scene

---
+++ Scripts/System/GameManager.cs
@@ -0,0 +1,48 @@
+using Godot;
+using System;
+
+public partial class GameManager : Node3D {
+
+	[Export]
+	private float _waitUntilLoad = 7f;
+
+	[Export]
+	private Node3D _gameContainer;
+
+	[Export]
+	private CanvasLayer _elementToHideOnStart;
+
+	private float _count;
+	private bool _loaded = false;
+	private Node _game = null;
+
+	public override void _Process(double delta) {
+		if (_loaded) {
+			return;
+		}
+		_count += (float) delta;
+		if (_count > _waitUntilLoad) {
+			_loaded = true;
+			_elementToHideOnStart.Visible = false;
+			LoadGame();
+		}
+	}
+
+	public void LoadGame() {
+		string path = "res://Scenes/world_tomeu.tscn";
+
+		if (ResourceLoader.Load(path) is PackedScene scene) {
+			_game = scene.Instantiate();
+			_gameContainer.AddChild(_game);
+		}
+	}
+
+	public void RestartGame() {
+		if (_game != null) {
+			_game.GetParent().RemoveChild(_game);
+			QueueFree();
+			LoadGame();
+		}
+	}
+	
+}
