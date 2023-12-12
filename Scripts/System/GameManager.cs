94050606d88145ff0483fc2fa4beca7b286e7917
Soteck <tukav73@gmail.com>
2023-12-12 17:42:35 +0100

    Add second splash screen

--- Scripts/System/GameManager.cs
+++ Scripts/System/GameManager.cs
@@ -4,27 +4,42 @@ using System;
 public partial class GameManager : Node3D {
 
 	[Export]
-	private float _waitUntilLoad = 7f;
+	private float _firstPanelTime = 3f;
+
+	[Export]
+	private float _secondPanelTime = 7f;
 
 	[Export]
 	private Node3D _gameContainer;
 
 	[Export]
-	private CanvasLayer _elementToHideOnStart;
+	private Panel _firstPanelToHide;
+
+	[Export]
+	private Panel _secondPanelToHide;
 
 	private float _count;
 	private bool _loaded = false;
 	private Node _game = null;
+
+	public override void _Ready() {
+		_firstPanelToHide.Visible = true;
+	}
 
 	public override void _Process(double delta) {
 		if (_loaded) {
 			return;
 		}
 		_count += (float) delta;
-		if (_count > _waitUntilLoad) {
-			_elementToHideOnStart.Visible = false;
+		if (_count > _firstPanelTime) {
+			_firstPanelToHide.Visible = false;
+			_secondPanelToHide.Visible = true;
+		}
+		if (_count > (_firstPanelTime + _secondPanelTime)) {
+			_secondPanelToHide.Visible = false;
 			LoadGame();
 		}
+		
 	}
 
 	public void LoadGame() {
